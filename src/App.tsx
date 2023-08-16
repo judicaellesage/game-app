7eed7f58101d30dd98d10e960f8c370aea7c7052
Judicael <judicojean@gmail.com>
2023-08-16 15:23:03 +0200

    write hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+import React, {FunctionComponent} from "react";
 
+const App: FunctionComponent = () => {
+  const name: string = "World";
+
+  return (
+    <h1>Hello {name}</h1>
+  );
+}
+
+export default App;
